80770c9160a1403aec089f75ad429a7770a5c65f
engrcollins <akinbamicollins@gmail.com>
2020-05-25 14:41:34 +0000

    Updates

---
+++ precache-manifest.95aa0c4d95b4c6a0503cf1b9ccd8df6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534921f43e51fc55c95a7ed2f84c1118",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "ae455479355f7e5e6559",
+    "url": "/e-commerce/static/css/main.e5709d2e.chunk.css"
+  },
+  {
+    "revision": "f7dce2f70bab94149cc8",
+    "url": "/e-commerce/static/js/2.23b1f098.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/e-commerce/static/js/2.23b1f098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae455479355f7e5e6559",
+    "url": "/e-commerce/static/js/main.59dcf6e1.chunk.js"
+  },
+  {
+    "revision": "8bf914ad80534231fef2",
+    "url": "/e-commerce/static/js/runtime-main.a838ffd2.js"
+  }
+]);
