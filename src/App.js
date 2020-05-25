5588d5c5ede9eeb279806c7c4c1733041a809ecc
engrcollins <akinbamicollins@gmail.com>
2020-05-25 14:34:09 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {component, useState} from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Container, Grid, Paper, Typography } from "@material-ui/core";
+import LayersIcon from '@material-ui/icons/Layers';
+//import "bootstrap/dist/css/bootstrap.min.css";
+import Home from './Home.js';
+import ArchiveNav from './Navigation';
+import Side from './SideNav.js';
+import "./App.css";
+import Registration from "./components/Register";
+import Login from "./components/Login";
+
+function App() {
+
+  return (
+      <div id="appContainer" >
+      <Router>
+          <ArchiveNav />
+                <Grid container spacing={3} component={Paper} >
+                <Grid item xs={12} sm={3} lg={2}>
+                  <Side />
+                </Grid>
+                  <br/>
+                  <br/>
+                  <Grid item xs={9} sm={9} lg={10}>
+                  <div className="appContent" style={{textAlign: "center"}}>
+                  <Switch>
+                      <Route path="/registration" component={Registration} />
+                      <Route path="/login" component={Login} />
+                      <Route path="/" component={Home} />
+                    </Switch>
+                </div>
+                </Grid>
+                </Grid>
+            <div>
+            </div>
+            </Router>
+          </div>
+  );
+}
+
+export default App;
